2abaa0373deb29a802fbc502a8d47661866dbbc0
vattanaeang <36014029+vattanaeang@users.noreply.github.com>
2025-10-14 21:52:32 -0400

    Final Submission for jQuery Assignment

--- scripts/jquery_assignment.js
+++ scripts/jquery_assignment.js
@@ -6,11 +6,11 @@ function GalleryItem(name, price, image, largeImage) {
 }
 
 var galleryItem = [
-    new GalleryItem("Ginevra de' Benci", 450000000, "../pictures/jQueryAssignment/pictures/Ginevra_de_Benci.jpeg", "../pictures/jQueryAssignment/pictures/Ginevra_de_Benci.jpeg"),
-    new GalleryItem("The Feast of the Gods", 200000, "../pictures/jQueryAssignment/pictures/The_Feast_of_the_Gods.jpeg", "../pictures/jQueryAssignment/pictures/The_Feast_of_the_Gods.jpeg"),
-    new GalleryItem("Family of Saltimbanques", 70000000, "../pictures/jQueryAssignment/pictures/Family_of_Saltimbanques.jpeg", "pictures/item1Large.jpg"),
-    new GalleryItem("The Shaw 54th Regiment Memorial", 95000000, "../pictures/jQueryAssignment/pictures/The_Shaw_54th_Regiment_Memorial.jpeg", "pictures/item1Large.jpg"),
-    new GalleryItem("Woman with a Sunflower", 12500000, "../pictures/jQueryAssignment/pictures/Woman_with_a_Sunflower.jpeg", "pictures/item1Large.jpg")
+    new GalleryItem("Ginevra de' Benci", 450000000, "../pictures/jQueryAssignment/pictures/Ginevra_de_Benci_small.jpeg", "../pictures/jQueryAssignment/pictures/Ginevra_de_Benci.jpeg"),
+    new GalleryItem("The Feast of the Gods", 200000, "../pictures/jQueryAssignment/pictures/The_Feast_of_the_Gods_small.jpeg", "../pictures/jQueryAssignment/pictures/The_Feast_of_the_Gods.jpeg"),
+    new GalleryItem("Family of Saltimbanques", 70000000, "../pictures/jQueryAssignment/pictures/Family_of_Saltimbanques_small.jpeg", "../pictures/jQueryAssignment/pictures/Family_of_Saltimbanques.jpeg"),
+    new GalleryItem("The Shaw 54th Regiment Memorial", 95000000, "../pictures/jQueryAssignment/pictures/The_Shaw_54th_Regiment_Memorial_small.jpeg", "../pictures/jQueryAssignment/pictures/The_Shaw_54th_Regiment_Memorial.jpeg"),
+    new GalleryItem("Woman with a Sunflower", 12500000, "../pictures/jQueryAssignment/pictures/Woman_with_a_Sunflower_small.jpeg", "../pictures/jQueryAssignment/pictures/Woman_with_a_Sunflower.jpeg")
 ]
 $(document).ready(function () {
     const $normalSize = $('#normalSize');
@@ -32,7 +32,6 @@ $(document).ready(function () {
 
         const $largeImg = $('<img>')
             .attr('src', item.largeImage)
-            .css('max-width', '400px')
             .hide();
 
         $largeSize.empty().append($largeImg);
@@ -48,7 +47,8 @@ $(document).ready(function () {
             .html(`<p><strong>${item.name}</strong><br>Price: $${item.price.toLocaleString()}</p>`)
             .fadeIn(500)
             .delay(4000)
-            .fadeOut(1000);
+            .fadeOut(1000)
+            .css('text-align', 'center');
     });
 
 });
